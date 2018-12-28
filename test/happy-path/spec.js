0c13b5a64ecc7c4493f32f293ac4934d6b5dfed3
Bart Veneman <bartveneman@users.noreply.github.com>
2018-12-28 20:32:35 +0100

    finish happy path tests

--- test/happy-path/spec.js
+++ test/happy-path/spec.js
@@ -8,7 +8,7 @@ const readFile = promisify(fs.readFile)
 test('it reports a success if all assertions pass with CSS via stdIn', async t => {
 	const [{stdout: actual}, expected] = await Promise.all([
 		execa('../../cli.js', {
-			input: 'body { color: blue; }',
+			input: 'body {\n\tcolor: blue;\n}\n',
 			cwd: __dirname
 		}),
 		readFile('./test/happy-path/expected-success.txt', {
@@ -23,30 +23,62 @@ test('it reports a success if all assertions pass with CSS via stdIn', async t =
 		.forEach(line => t.true(actual.includes(line)))
 })
 
-test.todo(
-	'it reports a success if all assertions pass with a CSS file path as an argument'
-)
-test('it reports a failure if some assertions are exceeded', async t => {
-	const [{code: exitCode, stdout: actual}, expected] = await Promise.all([
-		t.throwsAsync(
-			execa('../../cli.js', {
-				input: 'body { color: blue; margin: 0; }',
-				cwd: __dirname
-			})
-		),
-		readFile('./test/happy-path/expected-failure.txt', {
+test('it reports a success if all assertions pass with a CSS file path as an argument', async t => {
+	const [{stdout: actual}, expected] = await Promise.all([
+		execa('../../cli.js', ['fixture-success.css'], {
+			cwd: __dirname
+		}),
+		readFile('./test/happy-path/expected-success.txt', {
 			encoding: 'utf8'
 		})
 	])
 
-	// expected
-	// 	.split('\n')
-	// 	.map(line => line.replace(/#.*|^✔.*|^✖.*/, '').trim())
-	// 	.filter(line => line !== '')
-	// 	.forEach(line => {
-	// 		t.true(actual.includes(line))
-	// 	})
+	expected
+		.split('\n')
+		.map(line => line.replace(/#.*|^✔.*|^✖.*/, '').trim())
+		.filter(line => line !== '')
+		.forEach(line => t.true(actual.includes(line)))
+})
+
+test('it reports a failure if some assertions are exceeded', async t => {
+	const {code: exitCode, stdout: actual} = await t.throwsAsync(
+		execa('../../cli.js', {
+			input: 'body {\n\tcolor: blue;\n\tmargin: 0;\n}\n',
+			cwd: __dirname
+		})
+	)
+	;[
+		'not ok 1 - declarations.total should not be larger than 1 (actual: 2)',
+		'not ok 1 - declarations.totalUnique should not be larger than 1 (actual: 2)',
+		'not ok 1 - properties.total should not be larger than 1 (actual: 2)',
+		'not ok 1 - properties.totalUnique should not be larger than 1 (actual: 2)',
+		'not ok 47 - properties.unique',
+		'not ok 1 - stylesheets.cohesion.average should not be larger than 1 (actual: 2)',
+		'not ok 1 - stylesheets.size should not be larger than 23 (actual: 35)',
+		'not ok 1 - values.total should not be larger than 1 (actual: 2)',
+		'# failed 8 of 94 tests'
+	].forEach(subTestFailure => t.true(actual.includes(subTestFailure)))
 
 	t.is(exitCode, 1)
 })
-test.todo('it reports an error if no css is passed') // error code + stdOut
+
+test('it reports an error if no css is passed', async t => {
+	const {code: exitCode, stdout: actual} = await t.throwsAsync(
+		execa('../../cli.js', ['fixture-failure.css'], {
+			cwd: __dirname
+		})
+	)
+	;[
+		'not ok 1 - declarations.total should not be larger than 1 (actual: 2)',
+		'not ok 1 - declarations.totalUnique should not be larger than 1 (actual: 2)',
+		'not ok 1 - properties.total should not be larger than 1 (actual: 2)',
+		'not ok 1 - properties.totalUnique should not be larger than 1 (actual: 2)',
+		'not ok 47 - properties.unique',
+		'not ok 1 - stylesheets.cohesion.average should not be larger than 1 (actual: 2)',
+		'not ok 1 - stylesheets.size should not be larger than 23 (actual: 35)',
+		'not ok 1 - values.total should not be larger than 1 (actual: 2)',
+		'# failed 8 of 94 tests'
+	].forEach(subTestFailure => t.true(actual.includes(subTestFailure)))
+
+	t.is(exitCode, 1)
+})
