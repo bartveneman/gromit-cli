50bbc2727dada5570b38663b2d9cfd2778f73da8
Bart Veneman <bartveneman@users.noreply.github.com>
2018-12-29 13:54:06 +0100

    finish tests for config with comments

--- test/config-with-comments/spec.js
+++ test/config-with-comments/spec.js
@@ -1,6 +1,28 @@
+const {promisify} = require('util')
+const fs = require('fs')
 const test = require('ava')
+const execa = require('execa')
 
-test('it parses config with JSON comments withour problems', t => {
+const readFile = promisify(fs.readFile)
+
+test('it parses config with JSON comments without problems', async t => {
 	// read fixture config
 	// test happy path for thresholds and alternative
+	const [{code: exitCode, stdout: actual}, expected] = await Promise.all([
+		execa('../../cli.js', {
+			input: 'body {\n\tcolor: blue;\n}\n',
+			cwd: __dirname
+		}),
+		readFile('./test/config-with-comments/expected.txt', {
+			encoding: 'utf8'
+		})
+	])
+
+	expected
+		.split('\n')
+		.map(line => line.replace(/#.*|^✔.*|^✖.*/, '').trim())
+		.filter(line => line !== '')
+		.forEach(line => t.true(actual.includes(line)))
+
+	t.is(exitCode, 0)
 })
